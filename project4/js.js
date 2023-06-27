c8ca5c9a84e6dfabb1ee59e2d3be1c99b7eae179
vigneshwaran33555 <132059498+vigneshwaran33555@users.noreply.github.com>
2023-06-27 13:20:55 +0530

    Add files via upload
    https://moonlit-douhua-46898e.netlify.app/

---
+++ project4/js.js
@@ -0,0 +1,7 @@
+const light = document.querySelector('.light');
+
+window.addEventListener('mousemove',(e)=>{
+    light.style.setProperty('--x',e.clientX + 'px');
+    light.style.setProperty('--y',e.clientY + 'px');
+
+})
